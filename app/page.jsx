cbe036f7f4c26f4e2bce522ea70c7332bffadcda
Pradip Bedre <bedrepradip0@gmail.com>
2023-07-25 01:55:37 +0530

    auth compleated

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Feed from "@components/Feed";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br />
+        <span className="orange_gradient">AI-Powered Prompt</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+      <Feed />
+    </section>
+  );
+};
 
 export default page;
